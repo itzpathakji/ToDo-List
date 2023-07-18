b61cd6e9216274fb02294b895aab390412723ef4
Shubham Kumar Pathak <shubhampathak26092002@gmail.com>
2023-07-18 20:40:25 +0530

    Initialization

---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Header = () => {
+  return (
+    <nav>
+      Catch Your Daily Task.
+    </nav>
+  )
+}
+
+export default Header;
+
+
+
+
+
+
+
---
+++ src/Components/Home.jsx
@@ -0,0 +1,11 @@
+import React,{ useState } from 'react';
+
+const Home = () => {
+
+    const [inputValue,setInputValue] = useState(0);
+  return (
+    <div></div>
+  )
+};
+
+export default Home
